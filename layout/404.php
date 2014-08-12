668d2bace91fb0c2c98e284b1c9c8e303e91cc84
AlexCoderbit <sashayushchak@gmail.com>
2014-08-12 14:26:31 +0300

    404 latest I found after it was rewrote

--- layout/404.php
+++ layout/404.php
@@ -1,2 +1,6 @@
 <?php if ( !defined('MITH') ) {exit;} ?>
-<?php echo "404"; ?>
+<section class="container error404">
+    <img src="http://placehold.it/600x400" alt="" class="img-responsive">
+    <h1>We are sorry, but the page you are looking for is too old or unavailable</h1>
+    <h3>Please go back or start from the <a href="http://www.ethnocase.com">first page</a></h3>
+</section>
