79934ab155d70687df3087196cf30ba283af222c
AlexCoderbit <sashayushchak@gmail.com>
2014-08-12 13:40:47 +0300

    404

--- 404.php
+++
@@ -1,6 +0,0 @@
-<?php require_once("layout/head.php"); ?>
-<?php require_once("layout/header.php"); ?>
-
-    <img src="http://placehold.it/600x100" alt="" class="">
-    <h1>We are sorry, but the page you are looking for is too old or unavailable</h1>
-    <h3>Please go back or start from the <a href="index.php">first page</a></h3>
