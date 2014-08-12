3ba5a3c4495b6bb37db5fc5b2ac008b5de393858
Alexander <bars38@gmail.com>
2014-08-12 12:00:33 +0300

    cases fix

--- layout/cases.php
+++ layout/cases.php
@@ -7,26 +7,43 @@
 $menus = menu($lang, '2');
 $i = "0";
 
-foreach($menus as $data)
-    {
-        
-    	if ($data["parent"] == "0")
+if (empty($menus)) {
+    require_once("layout/404.php");
+    exit;
+}
+
+
+
+    foreach($menus as $data)
         {
-        	if ($i == '1'){ echo "</div>"; $i = '0'; }
-            echo '        <div class="catalog-brand">
-            <header class="headline">
-                <h1><img class="headlineImg '.$data["class"].'" src="'.$site.'img/cases/catModels/'.$data["img"].'" alt="'.$data["alt"].'"></h1>
-            </header>';
-        	$i = $i+1;
-        }else{
-echo '            <div class="brandModel">
-                <a href="'.$data["link_item"].'">
-                    <div class="brandPicWrap"><img src="'.$site.'img/cases/catModels/'.$data["img"].'" alt=""></div>
-                    <h2>'.$data[$lang].'</h2>
-                </a>
-            </div> ';
+            
+        	if ($data["parent"] == "0")
+            {
+            	if ($i == '1'){ echo "</div>"; $i = '0'; }
+                echo '        <div class="catalog-brand">
+                <header class="headline">
+                    <h1><img class="headlineImg '.$data["class"].'" src="'.$site.'img/cases/catModels/'.$data["img"].'" alt="'.$data["alt"].'"></h1>
+                </header>';
+            	$i = $i+1;
+                $parent = $data["id"];
+
+                    foreach($menus as $dat)
+                        {
+                            
+                            if ($dat["parent"] == $parent){
+                                echo '            <div class="brandModel">
+                                                    <a href="'.$dat["link_item"].'">
+                                                        <div class="brandPicWrap"><img src="'.$site.'img/cases/catModels/'.$dat["img"].'" alt=""></div>
+                                                        <h2>'.$dat[$lang].'</h2>
+                                                    </a>
+                                                </div> ';
+                            }
+                        }
+
+
+
+            }
         }
-    }
 
 ?>
 
