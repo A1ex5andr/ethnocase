bd0bd81a494ae7923708efec39d4f669590c2b37
Alexander <bars38@gmail.com>
2014-08-13 08:37:47 +0300

    fix cars

--- layout/cars.php
+++ layout/cars.php
@@ -1,92 +1,52 @@
 <?php if ( !defined('MITH') ) {exit;} ?>
 <?php 
 
+$menus = menu($lang, '3');
+$i = "0";
+
+if (empty($menus)) {
+    require_once("layout/404.php");
+    exit;
+}
+
+
 $cars = cars($lang);
 $name = "name_".$lang;
 $model = "model_".$lang;
 $about = "about_".$lang;
-?>
 
-    <section class="items container">
-        <div class="headline"><h2><?php echo $texts['cars18']; ?></h2></div>
+if (empty($cars)) {
+    require_once("layout/404.php");
+    exit;
+}
 
-<?php
-foreach($cars as $data)
+foreach($menus as $menu)
     {
-        if ($data["catalog"] == "18"){
-        echo '      <div class="itemBlock">
-                    <a href="'.$data["link_item"].'" class="itemBlockLink">
-                        <div class="itemPrice priceDiscount">
-                            <div class="itemPrice-final">'.$data["price"].'&#8372;</div>';
-        if ($data["price_old"] != '0'){echo '                    <div class="itemPrice-old">&nbsp;'.$data["price_old"].'&#8372;&nbsp;</div>';}
-        if ($data["disc"] != '0'){echo '                    <div class="itemPrice-disc">-'.$data["disc"].'%</div>';}    
-        echo '                </div>
-                        <div class="picWrap">
-                            <img class="picIndex" src="'.$site.'img/cars/'.$data["img"].'" alt="">
+
+    echo '    <section class="items container">
+        <div class="headline"><h2>'.$menu[$lang].'</h2></div>';
+
+        foreach($cars as $data)
+            {
+                if ($data["catalog"] == $menu["link_item"]){
+                echo '      <div class="itemBlock">
+                            <a href="'.$data["link_item"].'" class="itemBlockLink">
+                                <div class="itemPrice priceDiscount">
+                                    <div class="itemPrice-final">'.$data["price"].'&#8372;</div>';
+                if ($data["price_old"] != '0'){echo '                    <div class="itemPrice-old">&nbsp;'.$data["price_old"].'&#8372;&nbsp;</div>';}
+                if ($data["disc"] != '0'){echo '                    <div class="itemPrice-disc">-'.$data["disc"].'%</div>';}    
+                echo '                </div>
+                                <div class="stripeWrap">
+                                    <img class="stripeIndex" src="'.$site.'img/cars/'.$data["img"].'" alt="">
+                                </div>
+                            </a>
+                            <h3 class="itemName">'.$data[$name].'</h3>
+                            <h2 class="itemName">Вишиванка: наклейка на авто</h2>
                         </div>
-                    </a>
-                    <h3 class="itemName">'.$data[$name].'</h3>
-                    <h2 class="itemName">Вишиванка: наклейка на авто</h2>
-                </div>
-        ';
-        }
-    }
+                ';
+                }
+            }
 
+echo '  </section>';
+}
 ?>
-	</section>
-
-    <section class="items container">
-        <div class="headline"><h2><?php echo $texts['cars25']; ?></h2></div>
-
-<?php
-foreach($cars as $data)
-    {
-        if ($data["catalog"] == "25"){
-        echo '      <div class="itemBlock">
-                    <a href="'.$data["link_item"].'" class="itemBlockLink">
-                        <div class="itemPrice priceDiscount">
-                            <div class="itemPrice-final">'.$data["price"].'&#8372;</div>';
-        if ($data["price_old"] != '0'){echo '                    <div class="itemPrice-old">&nbsp;'.$data["price_old"].'&#8372;&nbsp;</div>';}
-        if ($data["disc"] != '0'){echo '                    <div class="itemPrice-disc">-'.$data["disc"].'%</div>';}    
-        echo '                </div>
-                        <div class="picWrap">
-                            <img class="picIndex" src="'.$site.'img/cars/'.$data["img"].'" alt="">
-                        </div>
-                    </a>
-                    <h3 class="itemName">'.$data[$name].'</h3>
-                    <h2 class="itemName">Вишиванка: наклейка на авто</h2>
-                </div>
-        ';
-        }
-    }
-
-?>
-    </section>
-
-	<section class="items container">
-	    <div class="headline"><h2><?php echo $texts['cars35']; ?></h2></div>
-
-<?php
-foreach($cars as $data)
-    {
-        if ($data["catalog"] == "35"){
-        echo '      <div class="itemBlock">
-                    <a href="'.$data["link_item"].'" class="itemBlockLink">
-                        <div class="itemPrice priceDiscount">
-                            <div class="itemPrice-final">'.$data["price"].'&#8372;</div>';
-        if ($data["price_old"] != '0'){echo '                    <div class="itemPrice-old">&nbsp;'.$data["price_old"].'&#8372;&nbsp;</div>';}
-        if ($data["disc"] != '0'){echo '                    <div class="itemPrice-disc">-'.$data["disc"].'%</div>';}    
-        echo '                </div>
-                        <div class="picWrap">
-                            <img class="picIndex" src="'.$site.'img/cars/'.$data["img"].'" alt="">
-                        </div>
-                    </a>
-                    <h3 class="itemName">'.$data[$name].'</h3>
-                    <h2 class="itemName">Вишиванка: наклейка на авто</h2>
-                </div>
-        ';
-        }
-    }
-
-?>
-    </section>
