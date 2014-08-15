dcdb4eae4f359f8811f5a91e7a844980fca4f1b9
AlexCoderbit <sashayushchak@gmail.com>
2014-08-15 09:22:30 +0300

    Case => Product

--- layout/cars_details.php
+++ layout/cars_details.php
@@ -43,25 +43,25 @@ foreach($cars as $data)
 
 		<div class="picsGallery">
 			<div class="picsGallery-thumbs">
-				<span><img src="'.$site.'img/cases/iphone_5/5_14_1.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleCasePic" id="1"></span>
-				<span><img src="'.$site.'img/cases/iphone_5/5_14_2.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleCasePic" id="2"></span>
-				<span><img src="'.$site.'img/cases/iphone_5/5_14_3.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleCasePic" id="3"></span>
-				<span><img src="'.$site.'img/cases/iphone_5/5_14_4.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleCasePic" id="4"></span>
+				<span><img src="'.$site.'img/cases/iphone_5/5_14_1.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleProductPic" id="1"></span>
+				<span><img src="'.$site.'img/cases/iphone_5/5_14_2.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleProductPic" id="2"></span>
+				<span><img src="'.$site.'img/cases/iphone_5/5_14_3.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleProductPic" id="3"></span>
+				<span><img src="'.$site.'img/cases/iphone_5/5_14_4.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleProductPic" id="4"></span>
 			</div>
 
 			<div class="picsGallery-show">
-				<img src="'.$site.'img/cars/'.$data["img"].'" class="singleCasePic" alt="Чехол для iPhone - этно чехол вышиванка на iPhone">						
+				<img src="'.$site.'img/cars/'.$data["img"].'" class="singleProductPic" alt="Чехол для iPhone - этно чехол вышиванка на iPhone">
 			</div>
 		</div>
 
-		<div class="singleCase">
+		<div class="singleProduct">
 			<header class="headline">
 				<h3>'.$texts['about_case'].'</h3>
 			</header>
 			<form action="#" class="buyForm">
 			    <button class="btn btn-buy"><i class="fa fa-shopping-cart"></i> купить</button>
 			</form>
-			<p class="singleCase-about">
+			<p class="singleProduct-about">
 				'.$data[$about].'
 			</p>
 			<div class="share42init"></div>
--- layout/cases_details.php
+++ layout/cases_details.php
@@ -43,25 +43,25 @@ foreach($cases as $data)
 
 		<div class="picsGallery">
 			<div class="picsGallery-thumbs">
-				<span><img src="'.$site.'img/cases/iphone_5/'.$data["link_item"].'_1.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleCasePic" id="1"></span>
-				<span><img src="'.$site.'img/cases/iphone_5/'.$data["link_item"].'_2.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleCasePic" id="2"></span>
-				<span><img src="'.$site.'img/cases/iphone_5/'.$data["link_item"].'_3.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleCasePic" id="3"></span>
-				<span><img src="'.$site.'img/cases/iphone_5/'.$data["link_item"].'_4.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleCasePic" id="4"></span>
+				<span><img src="'.$site.'img/cases/iphone_5/'.$data["link_item"].'_1.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleProductPic" id="1"></span>
+				<span><img src="'.$site.'img/cases/iphone_5/'.$data["link_item"].'_2.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleProductPic" id="2"></span>
+				<span><img src="'.$site.'img/cases/iphone_5/'.$data["link_item"].'_3.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleProductPic" id="3"></span>
+				<span><img src="'.$site.'img/cases/iphone_5/'.$data["link_item"].'_4.jpg" alt="Чехол для iPhone - этно чехол вышиванка на iPhone" class="singleProductPic" id="4"></span>
 			</div>
 
 			<div class="picsGallery-show">
-				<img src="'.$site.'img/cars/'.$data["img"].'" class="singleCasePic" alt="Чехол для iPhone - этно чехол вышиванка на iPhone">						
+				<img src="'.$site.'img/cars/'.$data["img"].'" class="singleProductPic" alt="Чехол для iPhone - этно чехол вышиванка на iPhone">
 			</div>
 		</div>
 
-		<div class="singleCase">
+		<div class="singleProduct">
 			<header class="headline">
 				<h3>'.$texts['about_case'].'</h3>
 			</header>
 			<form action="#" class="buyForm">
 			    <button class="btn btn-buy"><i class="fa fa-shopping-cart"></i> купить</button>
 			</form>
-			<p class="singleCase-about">
+			<p class="singleProduct-about">
 				'.$data[$about].'
 			</p>
 			<div class="share42init"></div>
