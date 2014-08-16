23bf94bdcb265f7235acb9eb29d379093a23eb46
Alexander <bars38@gmail.com>
2014-08-16 20:56:52 +0300

    cars done

--- admin/layout/cars.php
+++ admin/layout/cars.php
@@ -1,108 +1,124 @@
 <?php if ( !defined('MITH') ) {exit;} ?>
 <?php 
 
-// if ((!empty($form['link_items'])) && ($form['act'] == "add")) {
+if ((!empty($form['link_items'])) && ($form['act'] == "add")) {
 
-// 		//image upload
-// 		$filename = "img/news/".$form["link_items"]."-".basename($_FILES['img']['name']);
-// 		$img = $form["link_items"]."-".basename($_FILES['img']['name']);
+		//image upload
+		$filename = "img/cars/".$form["link_items"]."-".basename($_FILES['img']['name']);
+		$img = $form["link_items"]."-".basename($_FILES['img']['name']);
 
-// 		   if(is_uploaded_file($_FILES["img"]["tmp_name"]))
-// 		   {
-// 		     move_uploaded_file($_FILES["img"]["tmp_name"], $filename);
-// 		   }
-// 		//#image upload
+		   if(is_uploaded_file($_FILES["img"]["tmp_name"]))
+		   {
+		     move_uploaded_file($_FILES["img"]["tmp_name"], $filename);
+		   }
+		//#image upload
 
 
-// 	if ($form["top"] == "1") { $top = "1"; } else { $top = "0"; }
-// 	if ($form["active"] == "1") { $active = "1"; } else { $active = "0"; }
+	if ($form["active"] == "1") { $active = "1"; } else { $active = "0"; }
+	if ($form["price"] == "") { $price = "0"; } else { $price = $form["price"]; }
+	if ($form["price_old"] == "") { $price_old = "0"; } else { $price_old = $form["price_old"]; }
 
-// 		$database = new medoo();
+		$database = new medoo();
 
-// 		$last_user_id = $database->insert("news", [
-// 		"link_item" => $form["link_items"],
-// 		"img" => $img,
-// 		"eng" => $form["titlee"],
-// 		"rus" => $form["titler"],
-// 		"ukr" => $form["titleu"],
-// 		"eng_txt" => $form["texte"],
-// 		"rus_txt" => $form["textr"],
-// 		"ukr_txt" => $form["textu"],
-// 		"top" => $top,
-// 		"active" => $active
-// 		]);
+		$last_user_id = $database->insert("cars", [
+		"link_item" => $form["link_items"],
+		"img" => $img,
+		"catalog" => $form["catalog"],
+		"name_eng" => $form["namee"],
+		"name_rus" => $form["namer"],
+		"name_ukr" => $form["nameu"],
+		"model_eng" => $form["modele"],
+		"model_rus" => $form["modelr"],
+		"model_ukr" => $form["modelu"],
+		"about_eng" => $form["aboute"],
+		"about_ukr" => $form["aboutr"],
+		"about_rus" => $form["aboutu"],
+		"price" => $price,
+		"price_old" => $price_old,
+		"active" => $active
+		]);
+}
 
-// }elseif($form['act'] == "edit") {
+elseif($form['act'] == "edit") {
 
-// 	if (!empty($_FILES["img"]["tmp_name"])){
-// 		//image upload
-// 		$filename = "img/news/".$form["link_items"]."-".basename($_FILES['img']['name']);
-// 		$img = $form["link_items"]."-".basename($_FILES['img']['name']);
+	if (!empty($_FILES["img"]["tmp_name"])){
+		//image upload
+		$filename = "img/cars/".$form["link_items"]."-".basename($_FILES['img']['name']);
+		$img = $form["link_items"]."-".basename($_FILES['img']['name']);
 
-// 		   if(is_uploaded_file($_FILES["img"]["tmp_name"]))
-// 		   {
-// 		     move_uploaded_file($_FILES["img"]["tmp_name"], $filename);
-// 		   }
-// 		//#image upload
-// 	}else{
-// 		$img = $form["img_old"];
-// 	}
+		   if(is_uploaded_file($_FILES["img"]["tmp_name"]))
+		   {
+		     move_uploaded_file($_FILES["img"]["tmp_name"], $filename);
+		   }
+		//#image upload
+	}else{
+		$img = $form["img_old"];
+	}
 
-// 	if ($form["top"] == "1") { $top = "1"; } else { $top = "0"; }
-// 	if ($form["active"] == "1") { $active = "1"; } else { $active = "0"; }
+	if ($form["active"] == "1") { $active = "1"; } else { $active = "0"; }
+	if ($form["price"] == "") { $price = "0"; } else { $price = $form["price"]; }
+	if ($form["price_old"] == "") { $price_old = "0"; } else { $price_old = $form["price_old"]; }
 
-// 	$database = new medoo();
+	$database = new medoo();
 
-// 	$database->update("news", [
-// 		"link_item" => $form["link_items"],
-// 		"img" => $img,
-// 		"eng" => $form["titlee"],
-// 		"rus" => $form["titler"],
-// 		"ukr" => $form["titleu"],
-// 		"eng_txt" => $form["texte"],
-// 		"rus_txt" => $form["textr"],
-// 		"ukr_txt" => $form["textu"],
-// 		"top" => $top,
-// 		"active" => $active
-// 	], [
-// 	"id" => $form["id"]
-// 	]);
+	$database->update("cars", [
+		"link_item" => $form["link_items"],
+		"img" => $img,
+		"catalog" => $form["catalog"],
+		"name_eng" => $form["namee"],
+		"name_rus" => $form["namer"],
+		"name_ukr" => $form["nameu"],
+		"model_eng" => $form["modele"],
+		"model_rus" => $form["modelr"],
+		"model_ukr" => $form["modelu"],
+		"about_eng" => $form["aboute"],
+		"about_ukr" => $form["aboutr"],
+		"about_rus" => $form["aboutu"],
+		"price" => $price,
+		"price_old" => $price_old,
+		"active" => $active
+	], [
+	"id" => $form["id"]
+	]);
 
-// }elseif(!empty($form['links'])) {
+}elseif(!empty($form['links'])) {
 
-// 	if ($form["top"] == "1") { $top = "1"; } else { $top = "0"; }
-// 	if ($form["active"] == "1") { $active = "1"; } else { $active = "0"; }
+	if ($form["active"] == "1") { $active = "1"; } else { $active = "0"; }
 
-// 	$database = new medoo();
+	$database = new medoo();
 
-// 	$database->update("news", [
-// 		"top" => $top,
-// 		"active" => $active
-// 	], [
-// 	"link_item" => $form["links"]
-// 	]);
+	$database->update("cars", [
+		"active" => $active
+	], [
+	"link_item" => $form["links"]
+	]);
 
-// }
+}
 
 
 
 $cars = adm_cars();
+$menu = menu($lang, '3');
 
 
 if (!empty($loc["2"])){
-	$news_one = adm_news_one($lang, $loc["2"]);
-
-	$link_item = $news_one['0']['link_item'];
-	$img = '<input type="hidden" name="img_old" value="'.$news_one['0']['img'].'">';
-	$titleu = $news_one['0']['ukr'];
-	$titler = $news_one['0']['rus'];
-	$titlee = $news_one['0']['eng'];
-	$txtu = $news_one['0']['ukr_txt'];
-	$txtr = $news_one['0']['rus_txt'];
-	$txte = $news_one['0']['eng_txt'];
-	if ($news_one['0']['top'] == "1") { $top = "checked"; } else { $top = ""; }
-	if ($news_one['0']['active'] == "1") { $active = "checked"; } else { $active = ""; }
-	$y = '<input type="hidden" name="act" value="edit"><input type="hidden" name="id" value="'.$news_one['0']['id'].'">';
+	$cars_one = adm_cars_one($loc["2"]);
+	$link_item = $cars_one['0']['link_item'];
+	$img = '<input type="hidden" name="img_old" value="'.$cars_one['0']['img'].'">';
+	$catalog = $cars_one['0']['catalog'];
+	$name_eng = $cars_one['0']['name_eng'];
+	$name_ukr = $cars_one['0']['name_ukr'];
+	$name_rus = $cars_one['0']['name_rus'];
+	$model_eng = $cars_one['0']['model_eng'];
+	$model_ukr = $cars_one['0']['model_ukr'];
+	$model_rus = $cars_one['0']['model_rus'];
+	$about_eng = $cars_one['0']['about_eng'];
+	$about_ukr = $cars_one['0']['about_ukr'];
+	$about_rus = $cars_one['0']['about_rus'];
+	if ($cars_one['0']["price"] == "0") { $price = "0"; } else { $price = $cars_one['0']["price"]; }
+	if ($cars_one['0']["price_old"] == "0") { $price_old = "0"; } else { $price_old = $cars_one['0']["price_old"]; }
+	if ($cars_one['0']['active'] == "1") { $active = "checked"; } else { $active = ""; }
+	$y = '<input type="hidden" name="act" value="edit"><input type="hidden" name="id" value="'.$cars_one['0']['id'].'">';
 	
 	
 }else{
@@ -146,6 +162,19 @@ if (!empty($loc["2"])){
     		<p class="help-block">Example block-level help text here.</p></td>
 	</tr>
 	<tr>
+		<td>Catalog</td>
+		<td>
+			<select class="form-control" name="catalog">
+			  <?php
+			  	foreach ($menu as $menus) {
+			  		if ($menus["link_item"] == $catalog){ $cat = "selected"; } else { $cat = ""; }
+			  		echo '<option value="'.$menus["link_item"].'" '.$cat.'>'.$menus[$lang].'</option>';
+			  	}
+			  ?>
+			</select>
+		</td>
+	</tr>
+	<tr>
 		<td>Title</td>
 		<td>
 			<input name="nameu" type="text" class="form-control" placeholder="ukr" value="<?php echo $name_ukr; ?>"></br>
@@ -171,15 +200,11 @@ if (!empty($loc["2"])){
 	</tr>
 	<tr>
 		<td>Price</td>
-		<td><input name="price" type="checkbox" value="1" <?php echo $price; ?>></td>
+		<td><input name="price" type="text" class="form-control" placeholder="Price" value="<?php echo $price; ?>"></td>
 	</tr>
 	<tr>
 		<td>Price old</td>
-		<td><input name="price_old" type="checkbox" value="1" <?php echo $price_old; ?>></td>
-	</tr>
-	<tr>
-		<td>Price</td>
-		<td><input name="top" type="checkbox" value="1" <?php echo $top; ?>></td>
+		<td><input name="price_old" type="text" class="form-control" placeholder="Price old" value="<?php echo $price_old; ?>"></td>
 	</tr>
 	<tr>
 		<td>Active</td>
@@ -196,6 +221,7 @@ if (!empty($loc["2"])){
 	<tr>
 		<th>#</th>
 		<th>IMG</th>
+		<th>CATALOG</th>
 		<th>TEXT</th>
 		<th>PRICE</th>
 		<th>ACTIVE</th>
@@ -205,11 +231,13 @@ if (!empty($loc["2"])){
 foreach($cars as $data)
 	{
 		if ($data["active"] == "1") { $active = "checked"; } else { $active = ""; }
+		foreach ($menu as $menus) { if($data["catalog"] == $menus["link_item"]) { $menu_item = $menus[$lang]; } }
 		echo '	<tr>
 		<td><form role="form" method="post" enctype="multipart/form-data">'.$data["id"].'</td>
-		<td><img src="'.$site.'img/auto/'.$data["img"].'" width="50%" height="50%" alt=""></td>
+		<td><img src="'.$site.'img/cars/'.$data["img"].'" width="30%" height="30%" alt=""></td>
+		<td>'.$menu_item.'</td>
 		<td><a href="'.$asite.'cars/'.$data["id"].'">'.$data["name_ukr"].'</a></td>
-		<td>'.$data["price"].'</td>
+		<td>'.$data["price"].'</br>'.$data["price_old"].'</td>
 		<td><input type="checkbox" name="active" value="1" '.$active.'></td>
 		<td><input type="hidden" name="links" value="'.$data["link_item"].'"><button type="submit" class="btn btn-default">Submit</button></form></td>
 	</tr>';
