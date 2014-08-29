ba6fcd0b582eaf850c8da7a2cc788bd08770ab70
AlexCoderbit <sashayushchak@gmail.com>
2014-08-29 14:11:20 +0300

    after order

---
+++ thankyou.php
@@ -0,0 +1,11 @@
+<?php require_once("layout/head.php"); ?>
+<?php require_once("layout/header.php"); ?>
+
+
+    <div class="slogan container">
+        <h1>ДЯКУЭМО ЗА ВАШЕ ЗАМОВЛЕННЯ</h1>
+    </div>
+
+
+<?php require_once("layout/contacts.php"); ?>
+<?php require_once("layout/footer.php"); ?>
