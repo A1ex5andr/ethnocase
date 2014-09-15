37a26ee37ed76f27136805fbc1aedae09257c8e5
Alexander <bars38@gmail.com>
2014-09-15 13:07:03 +0300

    fix contact form

--- layout/contactSend.php
+++ layout/contactSend.php
@@ -1,34 +1,15 @@
+<?php if ( !defined('MITH') ) {exit;} ?>
 <?php
-echo "test";
-echo $form['contactname'];
 
-function redirect($time, $links, $text, $exit)
-    {
-        print"<html><head><meta http-equiv=\"refresh\" content=\"".$time."; url=".$links."\"></head>
-	        <body>
-		        ".$text."<p align=center><a href=\"".$links."\">"."</a><br><br>
-	        </body>
-        </html>"; 
-        if ($exit == '1'){exit;}
-    }
 
-function mailto($to,$subject,$message,$from)
-    {
-        $headers  = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-        $headers .= $from."\r\n";
-
-        mail($to, $subject, $message, $headers);  
-    }    
-
-    if( (!empty($form['contact-name']) && (!empty($form['contact-email'])) && (!empty($form['contact-message'])) ))
-		{
-			$message = '<html><head><title>ETHNOCASE - Contact</title></head><body>
+    if( (!empty($form['contact-name'])) && (!empty($form['contact-email'])) && (!empty($form['contact-message'])) )
+        {
+            $message = '<html><head><title>ETHNOCASE - Contact</title></head><body>
             <p>Автор: '.$form['contact-name']. '</p>
             <p>Email: '.$form['contact-email']. '</p><br />
-			<p>Вам надійшло замовлення:</p>
-			<p>'. $form['contact-message'] .'</p>
-			</body></html>';
+            <p>Вам надійшло замовлення:</p>
+            <p>'. $form['contact-message'] .'</p>
+            </body></html>';
 
             $message2 = "<html><head><title>ETHNOCASE</title></head><body>
             <p>Thank you, <br>
@@ -40,14 +21,24 @@ function mailto($to,$subject,$message,$from)
             ETHNOCASE Team
             </body></html>";
 
-			$from = "From: <bars38@gmail.com>\r\n";
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+        $headers .= 'From: bars38@gmail.com' . "\r\n" .
+    'Reply-To: bars38@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-            mailto('bars38@gmail.com', 'CoderStudio', $message, $from);
-			mailto($form['contact-email'], 'CoderStudio', $message2,  $from);
+        mail('bars38@gmail.com', 'CoderStudio', $message, $headers);
 
-            redirect('0', $link, '<!--h2 style="text-align: center; margin-top:100px;"> Thanks for registration!</h2-->', '1');
-            exit;
+
+
+print '<html><head><meta http-equiv="refresh" content="0; url='.$link.'"></head>
+            <body>
+                <!--h2 style="text-align: center; margin-top:100px;"> Thanks for registration!</h2--><p align=center><a href="'.$link.'"></a><br><br>
+            </body>
+        </html>';
+
+           exit;
         }
-?>		
+?>      
 
  
